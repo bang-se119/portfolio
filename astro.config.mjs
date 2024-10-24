817c3f8775d92d9523da6986f378c00fb778aed6
Dang Duy Bang <bangduy.se119@gmail.com>
2024-10-24 22:17:57 +0700

    deploy flow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react()],
+  integrations: [react()],
+  site: "https://bangdd.github.io",
+  base: "portfolio-astro",
 });
