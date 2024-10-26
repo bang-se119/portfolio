e2d7387f8bb4d1f8d3a0fb92acba804a3fee6249
Dang Duy Bang <bangduy.se119@gmail.com>
2024-10-26 22:25:24 +0700

    fix: site path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: "https://bangdd.github.io",
+  site: "https://bang-se119.github.io",
   base: "portfolio-astro",
   redirects: {
     '/': '/portfolio-astro',
