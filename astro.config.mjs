b193f653d502fc338f7a149af2744ee1334b30f9
Dang Duy Bang <bangduy.se119@gmail.com>
2024-10-27 09:48:00 +0700

    fix: title web

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [react()],
   site: "https://bang-se119.github.io",
-  base: "portfolio-astro",
+  base: "portfolio",
 });
